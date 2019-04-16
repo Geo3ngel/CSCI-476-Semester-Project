b3d97a8ce6feef559056bec989a365ce38f787d6
George <GeoEngel.z@gmail.com>
2019-04-16 02:18:32 -0600

    Implemented Mac address and IP address tracing
    -Also found out how to get the device name from IP
    -Needs to be cleaned up via regex still.

--- MAC_TEST.py
+++ MAC_TEST.py
@@ -3,6 +3,7 @@ import os
 import time
 import sys
 import subprocess
+import socket
 
 # TODO: Make command versions for linux and macOS in addition to Windows
 
@@ -39,14 +40,47 @@ class utils:
 
     # Attempts to run any string as a command
     def run_command(self, command):
-        
-        return subprocess.run([command], stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')
+        subprocess.Popen
+        return subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, env={'PATH': os.getenv('PATH')}).stdout.decode('utf-8')
+
+    def populate_MAC_Dict(self):
+        output = self.run_command(['arp', '-a'])
+        # TODO: Use regex to split up into a dict of IP's and MAC addresses, then build dicts with the results
 
 util = utils()
 
 # Tests the run_command method to check that it can in fact run a command & return the result
-print(util.run_command('dir'))
+print(util.run_command(['pip --help']))
+print(util.run_command(['spoof-mac.py list']))
 # TODO: npm install spoof and set up command line argument methods in here so it is effectivly put into python!
+#arp = os.system('arp -a')
+#os.system('spoof-mac.py list')
+
+print("TEST \n",util.run_command(['arp', '-a']))
+
+def get_Host_name_IP(): 
+    try: 
+        host_name = socket.gethostname() 
+        host_ip = socket.gethostbyname(host_name) 
+        print("Hostname :  ",host_name) 
+        print("IP : ",host_ip) 
+    except: 
+        print("Unable to get Hostname and IP") 
+
+get_Host_name_IP()
+
+# Try at shcool?
+
+#TODO: Set up so we get names aswell of known IP's?
+ip = '192.168.206.1'
+dns = socket.gethostbyaddr(ip)
+print("Name of device @:"+ip, dns[0])
+
+# Trying to associate a device name with a known ip
+#print("TEST \n",util.run_command(['nslookup', ip]))
+
+# TODO: Denies access due to the fact that is is under system 32. Solution? Move clone the spoof-mac.py project into here to use. (Only worry about this if trying to spoof MAC address)
+#print("TEST \n",util.run_command(['spoof-mac.py', 'list']))
 
 #pip install SpoofMAC
 #spoof-mac.py list
