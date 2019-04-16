a16873ba9f3f1c036537848c6762ac6f61245fd7
George <GeoEngel.z@gmail.com>
2019-04-16 00:26:52 -0600

    Updated for class like structure.

--- MAC_TEST.py
+++ MAC_TEST.py
@@ -1,49 +1,43 @@
 import netifaces
-import numpy as np
-import sys
 import os
 import time
 import sys
 
-def get_platform():
-    platforms = {
-        'linux1' : 'Linux',
-        'linux2' : 'Linux',
-        'darwin' : 'OS X',
-        'win32' : 'Windows'
-    }
-    if sys.platform not in platforms:
-        return sys.platform
-    
-    return platforms[sys.platform]
-
-print(netifaces.interfaces())
-
-# initalize empty dictionary
-mac_dict = {}
-
-# fill dictionary with MAC Addresses associated w/ numbers
-
-iter = 0
-
-# make data structure object instead for a dist of objects containing an IP, MAC, and Device Name
-for mac in netifaces.interfaces():
-    mac_dict[mac] = iter
-    iter+=1
-
-
-print(mac_dict)
-
-print("==================================================================================")
-
-def slowprint(s):
-    for c in s + '\n':
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        time.sleep(3. / 100)
-
-# Detects the operating system in use
-operating_system = get_platform()
-slowprint(operating_system + " operating system detected.")
-
 # TODO: Make command versions for linux and macOS in addition to Windows
+
+class utils:
+
+    def __init__(self, *args, **kwargs):
+        self.operating_system = self.get_platform()
+        self.slowprint(self.operating_system + " operating system detected.")
+
+        # initalize empty dictionary
+        self.mac_dict = {}
+
+        return super().__init__(*args, **kwargs)
+
+    # Informs us of what operating system is being used
+    def get_platform(self):
+        platforms = {
+            'linux1' : 'Linux',
+            'linux2' : 'Linux',
+            'darwin' : 'OS X',
+            'win32' : 'Windows'
+        }
+        if sys.platform not in platforms:
+            return sys.platform
+        
+        return platforms[sys.platform]
+
+    # Prints things slowly for style
+    def slowprint(self, s):
+        for c in s + '\n':
+            sys.stdout.write(c)
+            sys.stdout.flush()
+            time.sleep(3. / 100)
+
+    # Attempts to run any string as a command
+    def run_command(self, command):
+        os.system(command)
+
+util = utils()
